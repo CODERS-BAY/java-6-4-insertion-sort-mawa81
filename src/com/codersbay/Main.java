5b8955e7328d6d4131500d24fc29cd85be8b44d4
Mario Wagner <ma_wa@gmx.at>
2020-02-16 22:44:17 +0100

    Build first loops

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -3,7 +3,29 @@ package com.codersbay;
 public class Main {
 
     public static void main(String[] args) {
+        int[] list = {5, 4, 1, 9, 6, 7, 3, 2, 8};
+        int[] unsorted = new int[list.length];
+        int[] sorted = new int[list.length];
+        int count = 1;
+        int temp;
+        while (count < list.length) {
+            System.out.println(count);
+            for (int i = 1; i <= count; i++) {
+                if (list[i] < list[i - 1]) {
 
+                    temp = list[i];
+                    System.out.println("temp " + temp);
+                    list[i] = list[i - 1];
+                    list[i - 1] = temp;
+                    count++;
+                } else {
+                    System.out.println("else");
+                }
+            }
+        }
+        for (int p : list) {
+            System.out.println(p);
+        }
     }
 
 }
